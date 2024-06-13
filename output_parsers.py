9a9298603488244223b2935ffd193f23ccc2d8e8
Zhi Qiang QUEK <zhiqiang.quek@biposervice.com>
2024-06-13 17:44:15 +0800

    Added docstrings

--- output_parsers.py
+++ output_parsers.py
@@ -9,6 +9,11 @@ class Summary(BaseModel):
 
 
     def to_dict(self) -> Dict[str, Any]:
+        """Function which returns a dictionary containing defined fields (class variables) as key and its values.
+
+        Returns:
+            Dict[str, Any]: Dictionary containing defined class fields.
+        """
         return {"summary": self.summary, "facts": self.facts}
     
 # Schema of output required
