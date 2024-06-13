62e8681f49fdfcbaddf3e962b84fbc33497cd446
Zhi Qiang QUEK <zhiqiang.quek@biposervice.com>
2024-06-13 17:40:53 +0800

    Added TavilySearch API library for conducting search and acting as a tool for agent

--- tools/tools.py
+++ tools/tools.py
@@ -1,1 +1,13 @@
-from langchain_community.tools.tavily_search import TavilySearchResults  # Search API optimized for LLM and RAG. Limit to 1000Apis call/month
+# Search API optimized for LLM and RAG. Limit to 1000Apis call/month. Refer to https://docs.tavily.com/docs/tavily-api/introduction. 
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+def get_profile_url_via_tavily(name:str):
+    """Searches for Linkedin or X profile page.
+
+    Args:
+        name (str): String to be parsed as search value.
+    """
+    search = TavilySearchResults()
+    res = search.run(f"{name}")
+
+    return res[0]["url"]
